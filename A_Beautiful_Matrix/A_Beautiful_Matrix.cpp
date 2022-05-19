687e9c57727bcd6815f2995f172b2b1502c7b908
ROHIT RANJAN KUMAR <rohitranjankumar@pec.edu>
2022-05-19 12:04:23 +0530

    Add this file

---
+++ A_Beautiful_Matrix/A_Beautiful_Matrix.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int x,count ;
+    for(int i=1; i<=5 ; i++)
+    {
+        for(int j=1; j<=5 ; j++)
+        {
+            cin>>x;
+              if (x == 1)
+            {
+                cout << abs(i - 3) + abs(j - 3) << endl;
+            }
+        }
+    }
+
+    return 0;
+}
